f66927f2f0ebbc24cfab9a06ef39a79857693f0d
IvanFengJK <ivanfengjunkai1997114@gmail.com>
2022-12-13 18:03:12 +0800

    feat: able to create, read, update, delete

--- gateway/src/utils/WeaviateManager.py
+++ gateway/src/utils/WeaviateManager.py
@@ -1,13 +1,7 @@
-import yaml
 import weaviate
 import torch
-
-def read_yaml(file_path='config.yaml'):
-    with open(file_path, 'r') as f:
-        return yaml.safe_load(f)
-
-config = read_yaml()
-URL = config['database']['URL']
+import os
+import copy
 
 class VectorManager:
     def __init__(self) -> None:
@@ -20,10 +14,58 @@ class VectorManager:
         RETURNS: None
         ------------------------------------
         """
-        self._client = weaviate.Client('http://weaviate:8080/')
+        self._client = weaviate.Client(f"http://{os.environ.get('WEAVIATE_HOST')}:{os.environ.get('WEAVIATE_C_PORT')}")
+        
+    def _id2uuid(self, collection_name: str, id_no: str) -> str:
+        where_filter = {
+            'operator': 'Equal',
+            'valueText': id_no,
+            'path': ["id_no"]
+        }
+
+        query_result =  self._client.query.get(collection_name, ["id_no", "_additional {id}"]).with_where(where_filter).do()
+        uuid = query_result['data']['Get'][collection_name][0]['_additional']['id']
+        return uuid
+    
+    def _exists(self, id_no:str) -> bool:
+        where_filter = {
+            'operator': 'Equal',
+            'valueText': id_no,
+            'path': ['id_no']
+        }
+        response = self._client.query.get(collection_name, ["id_no", "_additional {id}"]).with_where(where_filter).do()
+        if len(response['data']['Get'][collection_name]) > 1:
+            print("There exist duplicated files")
+            return True
+        elif len(response['data']['Get'][collection_name]) == 1:
+            return True
+        return False
 
     def delete_collection(self, collection_name: str) -> None:
-        self._client.schema.delete_class(collection_name)
+        try:
+            self._client.schema.delete_class(collection_name)
+            print('Successfully deleted')
+        except Exception as e:
+            if '400' in str(e):
+                print('Collection does not exist so nothing to delete')
+            else:
+                print(f"Unknown error with error message -> {e}")
+            
+    def delete_document(self, collection_name: str, id_no: str) -> None:
+        # Check if the id exist
+        id_exists = self._exists(id_no)
+        
+        if not id_exists:
+            print("This id does not exist so no deletion is done")
+            return
+        
+        uuid = self._id2uuid(collection_name, id_no)
+        
+        try:
+            self._client.data_object.delete(uuid=uuid, class_name=collection_name)
+            print('Successfully deleted')
+        except Exception as e:
+            print(f"Unknown error with error message -> {e}")
 
     def create_collection(self, collection_name: str, schema: dict) -> None:
         """
@@ -50,7 +92,6 @@ class VectorManager:
         # Extract the document information into a list
         properties = []
         for key, val in schema['properties'].items():
-            print(key, val)
             properties.append({'name': key, 'dataType': val})
 
         # Proper formetting for creating the schema
@@ -63,12 +104,12 @@ class VectorManager:
         # Try to create schema. If exists, gracefully exit.
         try:
             self._client.schema.create_class(document_schema)
-            print("Collection successfilly created")
+            print("Collection Successfully created")
         except Exception as e:
             if '422' in str(e):
                 print("Collection has been created")
             else:
-                print(f"Unknwon error with error message -> {e}")
+                print(f"Unknown error with error message -> {e}")
 
 
     def create_document(self, collection_name: str, properties: dict, embedding: torch.Tensor) -> None:
@@ -97,17 +138,9 @@ class VectorManager:
             return
 
         # Check if the id exist
-        where_filter = {
-            'operator': 'Equal',
-            'valueText': properties.get('id_no'),
-            'path': ['id_no']
-        }
-
-        response = self._client.query.get(collection_name, ["id_no", "_additional {id}"]).with_where(where_filter).do()
-        if len(response['data']['Get'][collection_name]) > 1:
-            print("There exist duplicated files")
-            return
-        elif len(response['data']['Get'][collection_name]) == 1:
+        id_exists = self._exists(id_no)
+        
+        if id_exists:
             print("This id already existed please use update instead")
             return
 
@@ -133,23 +166,10 @@ class VectorManager:
         ------------------------------------
         """
         # Create filter and search
-        where_filter = {
-            'operator': 'Equal',
-            'valueText': id_no,
-            'path': ['id_no']
-        }
-        return self._client.query.get(collection_name, ["id_no", "_additional {vector}"]).with_where(where_filter).do()
+        uuid = self._id2uuid(collection_name, id_no)
+        return self._client.data_object.get_by_id(uuid = uuid, class_name = collection_name, with_vector = True)
 
-    def _id2uuid(self, collection_name, id):
-        where_filter = {
-            'operator': 'Equal',
-            'valueText': id,
-            'path': ["id_no"]
-        }
-
-        query_result =  self._client.query.get(collection_name, ["id_no", "_additional {id}"]).with_where(where_filter).do()
-        uuid = query_result['data']['Get'][collection_name][0]['_additional']['id']
-        return uuid
+    
 
     def update_document(self, collection_name: str, document: dict) -> None:
         """
@@ -171,15 +191,36 @@ class VectorManager:
         RETURNS: None
         ------------------------------------
         """
-        # if not document['properties'].get('id_no'):
-        #     print('Lack of id_no result in ambiguous document to update')
-        #     return
-        # self._client.data_object.update(
-        #     data_object = {}, 
-        #     class_name = collection_name, 
-        #     uuid = uuid, 
-        #     vector = new_vector
-        # )
-
-
-
+        if not document.get('id_no'):
+            print('Lack of id_no result in ambiguous document to update')
+            return
+        if not self._exists(document['id_no']):
+            print('Attempt to update a non-existent document. No update is done')
+            return
+        uuid = self._id2uuid(collection_name, document['id_no'])
+        temp = copy.deepcopy(document)
+        if 'vector' in document.keys():
+            new_vector = temp['vector']
+            if len(document.keys()) == 2:
+                self._client.data_object.update(
+                    data_object = {}, 
+                    class_name = collection_name, 
+                    uuid = uuid, 
+                    vector = new_vector
+                )
+            else:
+                del temp['id_no']
+                del temp['vector']
+                try:
+                    self._client.data_object.update(
+                        data_object = temp, 
+                        class_name = collection_name, 
+                        uuid = uuid, 
+                        vector = new_vector
+                    )
+                except Exception as e:
+                    if '400' in str(e):
+                        print("Unknown field(s) in document")
+                    else:
+                        print(f"Unknown error with error message -> {e}")
+                    
