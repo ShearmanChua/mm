13429e251ef52b9598117303b9768d0b6dc591bb
manekiyong <59855081+manekiyong@users.noreply.github.com>
2023-01-17 15:16:19 +0800

    update faceid submodule, upload ref faces

--- gateway/src/collection_setup.py
+++ gateway/src/collection_setup.py
@@ -1,14 +1,46 @@
+import json
+import os
+
 from utils.ESManager import DocManager
 from utils.WeaviateManager import VectorManager
+from viz_utils.img_server import ImageUploader
+from viz_utils.faceid import get_face_emb
 
 if __name__ == '__main__':
     DocMgr = DocManager()
     VecMgr = VectorManager()
 
-    es_mapping = {}
-    weaviate_schema = {'id_no': 'str'}
+    # M2E2 Schema
+    with open('../data/m2e2_map.json') as f:
+        m2e2_map = json.load(f)
+    m2e2_map = m2e2_map['documents_m2e2']['mappings']
 
-    DocMgr.create_collection('documents_m2e2', schema=es_mapping, custom_schema=True)
-    VecMgr.create_collection('face_db', schema=weaviate_schema)
+    # Wikipedia Schema
+    with open('../data/wiki_map.json') as f:
+        wiki_map = json.load(f)
+    wiki_map = wiki_map['wikipedia']['mappings']
+    
+    # Face Uploading Setup
+    facedb_schema = {'doc_id': 'str'}
+    ref_face_path = '../data/reference_faces'
 
-    # Populate face db 
+    DocMgr.create_collection('wikipedia', schema=wiki_map, custom_schema=True)
+    DocMgr.create_collection('documents_m2e2', schema=m2e2_map, custom_schema=True)
+    VecMgr.create_collection('face_db', schema=facedb_schema)
+
+    # Upload M2E2 source images
+    imu = ImageUploader()
+    imu.upload_all('../data/images_m2e2')
+
+    # Populate face ref db
+    for id in os.listdir(ref_face_path):
+        face_emb = get_face_emb(ref_face_path, id)
+        if face_emb['response']=='200':
+            facedb_doc = {
+                "doc_id": id,
+                "vector": face_emb['emb']
+            }
+            VecMgr.create_document('face_db', documents=facedb_doc)
+        else:
+            print(f"Failed to create {id}, no faces found")
+    
---
+++ gateway/src/viz_utils/img_server.py
@@ -0,0 +1,36 @@
+import base64
+import requests
+import os
+
+class ImageUploader:
+    def __init__(self):
+        self.server_address = 'image_server'
+        self.source_root = '../data/images_m2e2'
+
+    def upload_single_image(self, filename):
+
+        # full_name = os.path.join(self.source_root,filename+'.jpg')
+        full_name = os.path.join(self.source_root, filename)
+        print(full_name)
+
+        with open(full_name, "rb") as f:
+            im_bytes = f.read()
+        im_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+        body = {
+            'filename': filename.strip('.jpg'),
+            'image': im_b64
+        }
+        r = requests.put(
+            f'http://{self.server_address}:8000/upload/', json=body)
+
+        return r.json()['server_path']
+
+    def upload_all(self, data_root=None):
+        if data_root != None:
+            self.source_root = data_root
+        all_server_path = []
+        for i in os.listdir(self.source_root):
+            server_path = self.upload_single_image(i)
+            all_server_path.append(server_path)
+        return all_server_path
